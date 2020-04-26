4b26d8a6646df2bc9221e68f0589b0d84b68fa45
FAVOUR <truelyfavoured@gmail.com>
2020-04-26 06:37:51 +0100

    Instantiated Views in the MainActivity.

--- app/src/main/java/com/example/clickclickclick/MainActivity.kt
+++ app/src/main/java/com/example/clickclickclick/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.clickclickclick
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //Instantiating the views
+        val rootLayout: ConstraintLayout = findViewById(R.id.rootLayout)
+        val title: TextView = findViewById(R.id.title)
+        val taps: TextView = findViewById(R.id.taps)
+        val spinner: ProgressBar = findViewById(R.id.spinner)
     }
 }
