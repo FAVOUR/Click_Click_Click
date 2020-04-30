2d297a4cbb525a599b19e0aa83d0d7d7285265f6
FAVOUR <truelyfavoured@gmail.com>
2020-04-30 06:58:41 +0100

    Partially implemented test for coroutines

---
+++ app/src/test/java/com/example/clickclickclick/viewmodel/MainViewModelTest.kt
@@ -0,0 +1,20 @@
+package com.example.clickclickclick.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+class MainViewModelTest {
+
+//    @get:Rule
+//    val couroutineScope = MainC
+
+
+    @get:Rule
+    val instantTaskExecutorRule =InstantTaskExecutorRule()
+    @Before
+    fun setUp() {
+    }
+}
